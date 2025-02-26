da9f1f7f9cdc0b1159b3c6dc8f7c6e867777f664
deigosiahaan <deigosiahaan@gmail.com>
2025-02-26 23:38:23 +0700

    add authorization middleware

--- backend/app/internal/handler/employee_handler.go
+++ backend/app/internal/handler/employee_handler.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"github.com/deigo96/itineris/app/config"
 	"github.com/deigo96/itineris/app/internal/controller"
+	"github.com/deigo96/itineris/app/internal/middleware"
 	"github.com/deigo96/itineris/app/internal/service"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -13,6 +14,7 @@ func NewEmployeeHandler(config *config.Config, db *gorm.DB, router *gin.RouterGr
 
 	employeeController := controller.NewEmployeeController(employeeService)
 	employee := router.Group("/employees")
+	employee.Use(middleware.Authorization(config))
 
 	employee.GET("", employeeController.Get)
 
---
+++ backend/app/internal/middleware/authorization.go
@@ -0,0 +1,44 @@
+package middleware
+
+import (
+	"strings"
+
+	"github.com/deigo96/itineris/app/config"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func Authorization(config *config.Config) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenHeader := strings.Split(c.GetHeader("Authorization"), "Bearer ")
+		if len(tokenHeader) < 2 {
+			c.AbortWithStatusJSON(401, gin.H{
+				"code":    4005,
+				"message": "Unauthorized",
+			})
+			return
+		}
+
+		token, err := jwt.Parse(tokenHeader[1], func(t *jwt.Token) (interface{}, error) {
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrInvalidKey
+			}
+			return []byte(config.SecretKey), nil
+		})
+
+		if err != nil {
+			c.AbortWithStatusJSON(401, gin.H{
+				"code":    4005,
+				"message": "Unauthorized",
+			})
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("nip", claims["nip"])
+			c.Set("id", claims["id"])
+			c.Set("role", claims["role"])
+			c.Next()
+		}
+	}
+}
